2a9a0b184ca4b6615871af5969d5203d5e5d0079
Joshua Moon <josh.moon@gmail.com>
2019-04-12 10:37:22 -0400

    Add Promise all for Setup

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var gpio = require('rpi-gpio')
 var gpiop = gpio.promise;
 
-gpiop.setup(11, gpio.DIR_OUT).then(() => {
-	return gpiop.write(11, true)
+let setupPin11 = gpiop.setup(11, gpio.DIR_OUT);
+
+let setupPin12 = gpiop.setup(12, gpio.DIR_OUT);
+
+Promise.all([setupPin11, setupPin12]).then(() => {
+	gpio.write(11, false);
+	gpio.write(12, false);
 }).catch((err) => {
 	console.log('Error: ', err.toString())
 });
-
-gpiop.setup(12, gpio.DIR_OUT).then(() => {
-	return gpiop.write(12, true)
-}).catch((err) => {
-	console.log('Error: ', err.toString())
-})
