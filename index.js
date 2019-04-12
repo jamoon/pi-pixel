85ca88bacc5be1fa9e720a06714d49e1ad3e7ada
Joshua Moon <josh.moon@gmail.com>
2019-04-12 10:28:07 -0400

    Update Pin Numbering

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var gpio = require('rpi-gpio')
 var gpiop = gpio.promise;
 
 gpiop.setup(11, gpio.DIR_OUT).then(() => {
-	return gpiop.write(7, true)
+	return gpiop.write(11, true)
 }).catch((err) => {
 	console.log('Error: ', err.toString())
 });
 
 gpiop.setup(12, gpio.DIR_OUT).then(() => {
-	return gpiop.write(7, true)
+	return gpiop.write(12, true)
 }).catch((err) => {
 	console.log('Error: ', err.toString())
 })
