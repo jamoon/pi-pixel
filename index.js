56ea71b787930f9291478115b7b260d98f494f20
Joshua Moon <josh.moon@gmail.com>
2019-04-14 18:12:38 -0400

    Ben - Simplify COde

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 var gpio = require('rpi-gpio')
 var gpiop = gpio.promise;
 
-let delay = 1000;
 let onOff = true;
 
 let setupPin11 = gpiop.setup(11, gpio.DIR_OUT);
 let setupPin12 = gpiop.setup(12, gpio.DIR_OUT);
 
-Promise.all([setupPin11, setupPin12]).then(() => {
+let blink = function() {
+	onOff = !onOff;
+	gpio.write(11, onOff);
+	gpio.write(12, !onOff);
 
-	setInterval(function() {
-		onOff = !onOff;
-		gpio.write(11, onOff);
-		gpio.write(12, !onOff);
-	}, delay);
-	
+};
+
+Promise.all([setupPin11, setupPin12]).then(() => {
+	setInterval(blink, 1000);
 }).catch((err) => {
 	console.log('Error: ', err.toString())
 });
