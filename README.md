994731c1729cf71d2aa70295ec996787fbd6f6dd
Joshua Moon <josh.moon@gmail.com>
2019-04-15 18:46:27 -0400

    Update Blink logic

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # What is a Circuit?
 A circuit is a path between two or more points along which an electrical current can flow. The path may be closed (joined at both ends), making it a loop. A closed circuit makes electrical current flow possible. It may also be an open circuit where the electrical flow is cut short because the path is broken. An open circuit does not allow electrical current to flow.
-A circuit is a path between two or more points along which an electrical current can be carried
 
 # Resistors
 Resistors are electronic components that have a specific electrical resistance. The resistor's resistance limits the flow of electricity through a circuit.  Resistors are used to limit electrical current.  The electrical resistance of a resistor is measured in ohms. The symbol for an ohm is the greek capital-omega: Ω
-
 
 # Light-Emitting Diode
 An LED is an electronic device that emits light when an electrical current is passed through it.  They are normally used to tell if your circuit has power.
--- index.js
+++ index.js
@@ -10,53 +10,37 @@ let setupPin13 = gpiop.setup(13, gpio.DIR_OUT);
 let setupPin15 = gpiop.setup(15, gpio.DIR_OUT);
 let setupPin16 = gpiop.setup(16, gpio.DIR_OUT);
 
-let blink = function () {
+const blinkLight = function (pin) {
+	console.log(`Turn On Pin ${pin}`);
+	gpio.write(pin, on);
 
-	// BLINK GREEN
-	gpio.write(11, on);
-	gpio.write(11, off);
-
-	// BLINK RED
-	gpio.write(12, on);
-	gpio.write(12, off);
-
-	// BLINK YELLOW
-	gpio.write(13, on);
-	gpio.write(13, off);
-
-	// BLINK BLUE
-	gpio.write(15, on);
-	gpio.write(15, off);
-
-	// BLINK CLEAR
-	gpio.write(16, on);
-	gpio.write(16, off);
-};
-
+    return new Promise(resolve => {
+        setTimeout(() => {
+			console.log(`Turn Off Pin ${pin}`);
+			gpio.write(pin, off);
+            resolve();
+        }, 500);
+    });
+}
 
 const runLights = async () => {
 	while (true) {
 		console.log('Run Lights...');
-		
+
 		// BLINK GREEN
-		gpio.write(11, on);
-		gpio.write(11, off);
+		await blinkLight(11);
 
 		// BLINK RED
-		gpio.write(12, on);
-		gpio.write(12, off);
+		await blinkLight(12);
 
 		// BLINK YELLOW
-		gpio.write(13, on);
-		gpio.write(13, off);
+		await blinkLight(13);
 
 		// BLINK BLUE
-		gpio.write(15, on);
-		gpio.write(15, off);
+		await blinkLight(15);
 
 		// BLINK CLEAR
-		gpio.write(16, on);
-		gpio.write(16, off);
+		await blinkLight(16);
 	}
 }
 
@@ -69,5 +53,4 @@ Promise.all([
 	setupPin16
 ]).then(() => {
 	runLights();
-	// setInterval(blink, 1000);
 });
