05e5fc74b95abe57c5fb76cbcad013993fe1b387
Joshua Moon <josh.moon@gmail.com>
2019-04-14 21:11:36 -0400

    test async

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ let setupPin11 = gpiop.setup(11, gpio.DIR_OUT);
 let setupPin12 = gpiop.setup(12, gpio.DIR_OUT);
 let setupPin13 = gpiop.setup(13, gpio.DIR_OUT);
 let setupPin15 = gpiop.setup(15, gpio.DIR_OUT);
-let setupPin16 = gpiop.setup(16, gpio.DIR_OUT);	
+let setupPin16 = gpiop.setup(16, gpio.DIR_OUT);
 
-let blink = function() {
-	
+let blink = function () {
+
 	// BLINK GREEN
 	gpio.write(11, on);
 	gpio.write(11, off);
@@ -33,6 +33,32 @@ let blink = function() {
 	gpio.write(16, off);
 };
 
+
+const runLights = async () => {
+	while (true) {
+		// BLINK GREEN
+		gpio.write(11, on);
+		gpio.write(11, off);
+
+		// BLINK RED
+		gpio.write(12, on);
+		gpio.write(12, off);
+
+		// BLINK YELLOW
+		gpio.write(13, on);
+		gpio.write(13, off);
+
+		// BLINK BLUE
+		gpio.write(15, on);
+		gpio.write(15, off);
+
+		// BLINK CLEAR
+		gpio.write(16, on);
+		gpio.write(16, off);
+	}
+}
+
+
 Promise.all([
 	setupPin11,
 	setupPin12,
@@ -40,5 +66,6 @@ Promise.all([
 	setupPin15,
 	setupPin16
 ]).then(() => {
-	setInterval(blink, 1000);
+	runLights();
+	// setInterval(blink, 1000);
 });
